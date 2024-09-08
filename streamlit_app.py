7f131d6c52c1413aa936e3d86abf9a37d1ae5bf1
PrashanthphData <pakurathi@phdata.io>
2024-09-08 22:48:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,14 @@ if ing_list:
     
     st.write(my_insert_stmt)
     # st.stop()
+
+    
     time_to_insert = st.button('Submit Order ')
     # st.write(my_insert_stmt)
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width = True)
